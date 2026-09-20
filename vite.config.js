3b0a5b0b848969dec6955a984a05a648bc0aff19
rawan <rewanelkady141@gmail.com>
2026-09-21 01:43:42 +0300

    chore: change base url for the project

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/responsive-social-website/',
+  base: '/wavelength/',
   plugins: [react(), tailwindcss()],
 })
